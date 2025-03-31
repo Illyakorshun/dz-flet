835569b06ac6f57aa4f415b18238e8b4602d0b1c
Korshun Ilya Oleksandrovych <116439677+Illyakorshun@users.noreply.github.com>
2025-03-31 09:35:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,17 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    return n * factorial(n - 1)
+
+try:
+    num = int(input("Введіть число: "))
+    if num < 0:
+        print("Факторіал визначений тільки для невід'ємних чисел.")
+    else:
+        print(f"Факторіал {num} дорівнює {factorial(num)}")
+except ValueError:
+    print("Будь ласка, введіть ціле число.")
